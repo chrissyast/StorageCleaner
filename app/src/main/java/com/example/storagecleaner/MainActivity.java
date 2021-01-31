66ec510565c3948c61ddf0e5e7b7f85a1cea2cfd
chrissyast <cjastles@gmail.com>
2021-01-31 16:21:35 +0000

    remove redundant code

--- app/src/main/java/com/example/storagecleaner/MainActivity.java
+++ app/src/main/java/com/example/storagecleaner/MainActivity.java
@@ -85,6 +85,7 @@ public void onActivityResult(int requestCode, int resultCode,
 
        @Override
        public void onSuccess() {
+           System.out.println("success!");
          // On Compression success
        }
 
@@ -124,153 +125,13 @@ public void onActivityResult(int requestCode, int resultCode,
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-      //  if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-          // Uri foo = Uri.parse("package:"+BuildConfig.APPLICATION_ID);
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
-
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
-
-    // Provide read access to files and sub-directories in the user-selected
-    // directory.
         intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-
-    // Optionally, specify a URI for the directory that should be opened in
-    // the system file picker when it loads.
-        //intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, uriToLoad);
-
         startActivityForResult(intent, 1);
       //  }
     }
 
-   /* @Override
-    public void onSensorChanged(SensorEvent event) {
-        System.out.println("x" + event.values[0]);
-        System.out.println("y" + event.values[1]);
-        System.out.println("z" + event.values[2]);
 
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }*/
-
-    public static String getPath(final Context context, final Uri uri) {
-
-
-    // DocumentProvider
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && DocumentsContract.isDocumentUri(context, uri)) {
-
-        if (isExternalStorageDocument(uri)) {
-            final String docId = DocumentsContract.getDocumentId(uri);
-            final String[] split = docId.split(":");
-            final String type = split[0];
-
-            return Environment.getExternalStorageDirectory() + "/" + split[1];
-
-            // TODO handle non-primary volumes
-        }
-        // DownloadsProvider
-        else if (isDownloadsDocument(uri)) {
-            try {
-                final String id = DocumentsContract.getDocumentId(uri);
-                //Log.d(TAG, "getPath: id= " + id);
-                final Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
-                return getDataColumn(context, contentUri, null, null);
-            }catch (Exception e){
-                e.printStackTrace();
-                List<String> segments = uri.getPathSegments();
-                if(segments.size() > 1) {
-                    String rawPath = segments.get(1);
-                    if(!rawPath.startsWith("/")){
-                        return rawPath.substring(rawPath.indexOf("/"));
-                    }else {
-                        return rawPath;
-                    }
-                }
-            }
-        }
-        // MediaProvider
-        else if (isMediaDocument(uri)) {
-            final String docId = DocumentsContract.getDocumentId(uri);
-            final String[] split = docId.split(":");
-            final String type = split[0];
-
-            Uri contentUri = null;
-            if ("image".equals(type)) {
-                contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-            } else if ("video".equals(type)) {
-                contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-            } else if ("audio".equals(type)) {
-                contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-            }
-
-            final String selection = "_id=?";
-            final String[] selectionArgs = new String[]{
-                    split[1]
-            };
-
-            return getDataColumn(context, contentUri, selection, selectionArgs);
-        }
-    }
-    // MediaStore (and general)
-    else if ("content".equalsIgnoreCase(uri.getScheme())) {
-
-        // Return the remote address
-        if (isGooglePhotosUri(uri))
-            return uri.getLastPathSegment();
-
-        return getDataColumn(context, uri, null, null);
-    }
-    // File
-    else if ("file".equalsIgnoreCase(uri.getScheme())) {
-        return uri.getPath();
-    }
-
-    return null;
-}
-public static String getDataColumn(Context context, Uri uri, String selection,
-                                   String[] selectionArgs) {
-
-    Cursor cursor = null;
-    final String column = "_data";
-    final String[] projection = {
-            column
-    };
-
-    try {
-        cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
-                null);
-        if (cursor != null && cursor.moveToFirst()) {
-
-
-            final int column_index = cursor.getColumnIndexOrThrow(column);
-            return cursor.getString(column_index);
-        }
-    }catch (Exception e){
-        e.printStackTrace();
-    }finally {
-        if (cursor != null)
-            cursor.close();
-    }
-    return "nodata";
-}
-
-public static boolean isExternalStorageDocument(Uri uri) {
-    return "com.android.externalstorage.documents".equals(uri.getAuthority());
-}
-
-public static boolean isDownloadsDocument(Uri uri) {
-    return "com.android.providers.downloads.documents".equals(uri.getAuthority());
-}
-
-public static boolean isMediaDocument(Uri uri) {
-    return "com.android.providers.media.documents".equals(uri.getAuthority());
-}
-
-public static boolean isGooglePhotosUri(Uri uri) {
-    return "com.google.android.apps.photos.content".equals(uri.getAuthority());
-}
 }
 
